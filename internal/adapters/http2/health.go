f3fdcecf9c97e402469792c0b9b5a6f0e7f03651
Aleksei Devyatkin <aleksei@devyatkin.dev>
2022-09-20 23:04:11 +0300

    added health check srv timeout

--- internal/adapters/http2/health.go
+++ internal/adapters/http2/health.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"net/http"
 	"tag-value-finder/internal/domain/errors"
+	"time"
 
 	"github.com/rs/zerolog/log"
 )
@@ -15,7 +16,13 @@ func HealthCheckHandler(ctx context.Context, addr string) error {
 		http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 			_, _ = w.Write([]byte("ok"))
 		})
-		err := http.ListenAndServe(addr, nil)
+		srv := &http.Server{
+			ReadTimeout:  5 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			Addr:         addr,
+		}
+
+		err := srv.ListenAndServe()
 		if err != nil {
 			log.Fatal().Msgf(errors.HTTPHealthListenError, err)
 		}
