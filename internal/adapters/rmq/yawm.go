ef6ee00326114723a7f91bca1b7820ad50b62a65
Aleksei <63240599+devalv@users.noreply.github.com>
2022-12-03 00:15:42 +0300

    some local updates (#1)
    another try of vscode as new local editor

--- internal/adapters/rmq/yawm.go
+++ internal/adapters/rmq/yawm.go
@@ -4,11 +4,11 @@ import (
 	"context"
 	"time"
 
-	"github.com/devalv/tag-value-finder/internal/domain/crawler"
-	"github.com/devalv/tag-value-finder/internal/domain/errors"
-
 	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/rs/zerolog/log"
+
+	"github.com/devalv/tag-value-finder/internal/domain/crawler"
+	"github.com/devalv/tag-value-finder/internal/domain/errors"
 )
 
 type YawmRmq struct {
